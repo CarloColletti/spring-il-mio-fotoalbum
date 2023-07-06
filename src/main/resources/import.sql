e54a5b0a47ddd1942514c49b119e343b21b54726
CarloColletti~ <carlocolletti5@gmail.com>
2023-07-06 14:39:03 +0200

    import categorie

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO `categories` (`id`, `description`, `name`) VALUES (NULL, 'Cute animals', 'Adorable');
+INSERT INTO `categories` (`id`, `description`, `name`) VALUES (NULL, 'Majestic landscapes', 'Breathtaking');
+INSERT INTO `categories` (`id`, `description`, `name`) VALUES (NULL, 'Delicious cuisine', 'Mouthwatering');
+INSERT INTO `categories` (`id`, `description`, `name`) VALUES (NULL, 'Extreme sports', 'Thrilling');
+INSERT INTO `categories` (`id`, `description`, `name`) VALUES (NULL, 'Modern architecture', 'Striking');
+INSERT INTO `categories` (`id`, `description`, `name`) VALUES (NULL, 'Abstract art', 'Expressive');
+INSERT INTO `categories` (`id`, `description`, `name`) VALUES (NULL, 'Romantic moments', 'Intimate');
+INSERT INTO `categories` (`id`, `description`, `name`) VALUES (NULL, 'Exciting board games', 'Competitive');
+INSERT INTO `categories` (`id`, `description`, `name`) VALUES (NULL, 'Powerful automobiles', 'Fast');
+INSERT INTO `categories` (`id`, `description`, `name`) VALUES (NULL, 'Elegant attire', 'Sophisticated');

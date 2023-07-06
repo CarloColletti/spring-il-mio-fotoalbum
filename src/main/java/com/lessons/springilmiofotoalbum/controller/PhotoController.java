cb0c3fc93890c729d16b3ba9e4bf6ec2c43463a0
CarloColletti~ <carlocolletti5@gmail.com>
2023-07-06 15:41:19 +0200

    add homepage(basic)

---
+++ src/main/java/com/lessons/springilmiofotoalbum/controller/PhotoController.java
@@ -0,0 +1,14 @@
+package com.lessons.springilmiofotoalbum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class PhotoController {
+    @GetMapping
+    public String index() {
+        return "home";
+    }
+}

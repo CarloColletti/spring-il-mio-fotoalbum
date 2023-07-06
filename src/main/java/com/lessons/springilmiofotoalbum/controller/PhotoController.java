e5ea3ebf154d4702ca2f475ab91ca70dc789840c
CarloColletti~ <carlocolletti5@gmail.com>
2023-07-06 17:15:38 +0200

    add index basic and population database(to do)

--- src/main/java/com/lessons/springilmiofotoalbum/controller/PhotoController.java
+++ src/main/java/com/lessons/springilmiofotoalbum/controller/PhotoController.java
@@ -1,14 +1,30 @@
 package com.lessons.springilmiofotoalbum.controller;
 
+import com.lessons.springilmiofotoalbum.model.Photo;
+import com.lessons.springilmiofotoalbum.repository.PhotoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/")
+@RequestMapping("/photo")
 public class PhotoController {
+    @Autowired
+    private PhotoRepository photoRepository;
+
     @GetMapping
-    public String index() {
-        return "home";
+    public String index(Model model) {
+        //prendo la lista delle foto
+        List<Photo> photos = photoRepository.findAll();
+        //le inserisco nel model
+        model.addAttribute("photo", photos);
+        return "/photo/index";
     }
+
+    //metodi___________________________________
+
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
+--action for categories table
 INSERT INTO `categories` (`id`, `description`, `name`) VALUES (NULL, 'Cute animals', 'Adorable');
 INSERT INTO `categories` (`id`, `description`, `name`) VALUES (NULL, 'Majestic landscapes', 'Breathtaking');
 INSERT INTO `categories` (`id`, `description`, `name`) VALUES (NULL, 'Delicious cuisine', 'Mouthwatering');
@@ -8,3 +9,39 @@ INSERT INTO `categories` (`id`, `description`, `name`) VALUES (NULL, 'Romantic m
 INSERT INTO `categories` (`id`, `description`, `name`) VALUES (NULL, 'Exciting board games', 'Competitive');
 INSERT INTO `categories` (`id`, `description`, `name`) VALUES (NULL, 'Powerful automobiles', 'Fast');
 INSERT INTO `categories` (`id`, `description`, `name`) VALUES (NULL, 'Elegant attire', 'Sophisticated');
+
+--action for photo table
+INSERT INTO `photos` (`id`, `title`, `description`, `url`, `isPost`) VALUES (NULL, 'Serenità al tramonto', 'Spiaggia dorata, mare calmo, sole arancione che si abbassa', 'https://previews.123rf.com/images/glebstock/glebstock1701/glebstock170100028/70415741-serenit%C3%A0-e-praticare-lo-yoga-al-tramonto-la-meditazione.jpg', 'TRUE');
+INSERT INTO `photos` (`id`, `title`, `description`, `url`, `isPost`) VALUES (NULL, '', '', '', 'TRUE');
+
+
+
+INSERT INTO `photos` (`id`, `title`, `description`, `url`, `isPost`) VALUES (NULL, 'Energia urbana', 'Grattacieli imponenti, traffico frenetico, luci notturne scintillanti', 'https://anteritalia.org/wp-content/uploads/2019/11/iStock-993560630.jpg', 'TRUE');
+
+
+    Titolo: "Energia urbana"
+    Descrizione: Grattacieli imponenti, traffico frenetico, luci notturne scintillanti.
+
+    Titolo: "Armonia naturale"
+    Descrizione: Cascata rugente, foresta rigogliosa, arcobaleno sublime.
+
+    Titolo: "Momenti familiari"
+    Descrizione: Tavolo imbandito, sorrisi contagiosi, abbracci affettuosi.
+
+    Titolo: "Avventura selvaggia"
+    Descrizione: Cime montuose, sentieri tortuosi, animali selvatici curiosi.
+
+    Titolo: "Riflessi d'autunno"
+    Descrizione: Foglie dorate, lago tranquillo, cielo azzurro riflesso.
+
+    Titolo: "Vita in città"
+    Descrizione: Mercato vivace, artisti di strada, grida di gioia.
+
+    Titolo: "Paesaggio incantato"
+    Descrizione: Neve scintillante, abeti imponenti, silenzio pacifico.
+
+    Titolo: "Espressioni uniche"
+    Descrizione: Volto rugoso, occhi penetranti, sorriso saggio.
+
+    Titolo: "Magia delle onde"
+    Descrizione: Spiaggia deserta, onde schiumose, brezza salata.

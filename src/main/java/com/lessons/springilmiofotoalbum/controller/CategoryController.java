8fc2b6954384ce4f92c23c948138b5c0474d9907
CarloColletti~ <carlocolletti5@gmail.com>
2023-07-10 01:57:14 +0200

    category list page

---
+++ src/main/java/com/lessons/springilmiofotoalbum/controller/CategoryController.java
@@ -0,0 +1,31 @@
+package com.lessons.springilmiofotoalbum.controller;
+
+import com.lessons.springilmiofotoalbum.model.Category;
+import com.lessons.springilmiofotoalbum.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/category")
+public class CategoryController {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    //INDEX -> lista delle categorie_____________
+    @GetMapping
+    public String index(Model model) {
+        //prendo la lista delle category
+        List<Category> categories = categoryRepository.findAll();
+        //le inserisco nel model
+        model.addAttribute("categories", categories);
+        model.addAttribute("newCategory", new Category());
+        return "/category/listCategory";
+    }
+
+
+}
